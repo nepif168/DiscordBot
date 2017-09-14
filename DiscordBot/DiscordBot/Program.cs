e8b43a7dedfcc35f070d20661a735963dc6a0548
Icchan <nepif168@gmail.com>
2017-09-14 15:56:08 +0900

    Commandの変数とか追加（初期化はまだ）

--- DiscordBot/DiscordBot/Program.cs
+++ DiscordBot/DiscordBot/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        // DiscordClient and options...
+        DiscordSocketClient client;
+        CommandService commands;
+        IServiceProvider services;
+
+        // Main => MainAsync
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        // Main
+        public async Task MainAsync()
         {
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            client = new DiscordSocketClient(_config);
+
+            services = new ServiceCollection().BuildServiceProvider();
+
+            await client.LoginAsync(TokenType.Bot, "bot token");
+            await client.StartAsync();
+
+            client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.FromResult(0);
+            };
+
+            await Task.Delay(-1);
         }
     }
 }
