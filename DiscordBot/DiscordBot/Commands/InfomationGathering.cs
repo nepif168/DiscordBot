4b0319271fcb54d606ee4a174c8527c190b7afbd
Icchan <nepif168@gmail.com>
2017-09-16 15:03:47 +0900

    Youtubeコマンド追加

---
+++ DiscordBot/DiscordBot/Commands/InfomationGathering.cs
@@ -0,0 +1,79 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace DiscordCommand
+{
+    [Group("twitter")]
+    public class TwitterCommand : ModuleBase
+    {
+        [Command("ranking"), Summary("twitter ranking")]
+        public async Task Ranking([Summary("Tweet ranking")] int amount = 3)
+        {
+            WebClient wc = new WebClient();
+            wc.Encoding = System.Text.Encoding.UTF8;
+            wc.Headers.Add("Accept-Language", "ja"); //日本語要求
+            wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            string html = wc.DownloadString("https://tr.twipple.jp/tweet/");
+
+            Regex reg = new Regex("<a href=\"(?<url>.*?)\".*?>(?<text>.*?)</a>",
+                            RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            int i = 0;
+            for (Match m = reg.Match(html); m.Success; m = m.NextMatch())
+            {
+                string url = m.Groups["url"].Value;
+                if (url.IndexOf("/status/") > 0)
+                {
+                    i++;
+                    await ReplyAsync(url);
+                    if (i == amount)
+                        break;
+                }
+            }
+        }
+    }
+
+    [Group("2ch")]
+    public class Ch2 : ModuleBase
+    {
+
+    }
+
+    [Group("youtube")]
+    public class YouTube : ModuleBase
+    { 
+        [Command("ranking")]
+        public async Task Ranking(int amount = 3)
+        {
+            WebClient wc = new WebClient();
+            wc.Encoding = System.Text.Encoding.UTF8;
+            wc.Headers.Add("Accept-Language", "ja"); //日本語要求
+            wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            string html = wc.DownloadString("https://www.youtube.com/feed/trending?hl=ja&gl=JP");
+
+            Regex reg = new Regex("<a href=\"(?<url>.*?)\".*?>(?<text>.*?)</a>",
+                            RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            int i = 0;
+            for (Match m = reg.Match(html); m.Success; m = m.NextMatch())
+            {
+                string url = m.Groups["url"].Value;
+                if (url.IndexOf("/status/") > 0)
+                {
+                    i++;
+                    await ReplyAsync(url);
+                    if (i == amount)
+                        break;
+                }
+            }
+    }
+}
